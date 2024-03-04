83093a5183583580040efeb6b9f1fbca010030a6
Raulalberto1 <raul.alberto.91@gmail.com>
2024-03-04 17:41:14 -0600

    first commit

---
+++ src/main/java/com/programming/productservice/config/DatabaseChangeLog.java
@@ -0,0 +1,13 @@
+package com.programming.productservice.config;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+
+@ChangeLog
+public class DatabaseChangeLog {
+
+	//@ChangeSet
+	public void seedDatabase() {
+		
+	}
+}
---
+++ src/main/java/com/programming/productservice/dto/ProductRequest.java
@@ -0,0 +1,23 @@
+package com.programming.productservice.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductRequest {
+
+	private String name;
+	private String description;
+	private BigDecimal price;
+}
